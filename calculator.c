312b000e1a8eb917f221bf18e4c5fcd033357e77
parth-kaushal <122971445+parth-kaushal@users.noreply.github.com>
2023-01-24 11:39:12 +0530

    new file

--- calculator.c
+++ calculator.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	char op;
-	printf("Enter the operation you want to perform : ");
-	scanf("%c",&op);
+    char op;
+    printf("Enter the operation you want to perform : ");
+    scanf("%c", &op);
 
-	float num1,num2;
-	printf("Enter first number : ");
-	scanf("%f",&num1);
-	printf("Enter second number : ");
-	scanf("%f",&num2);
+    float num1, num2;
+    printf("Enter first number : ");
+    scanf("%f", &num1);
+    printf("Enter second number : ");
+    scanf("%f", &num2);
     printf("Answer : ");
-	switch(op)
-	{
-    		case '+':
-			printf("%.3f",num1+num2);
-			break;
-		case '-':
-			printf("%.3f",num1-num2);
-			break;
-		case '*':
-			printf("%.3f",num1*num2);
-			break;
-		case '/':
-			printf("%.3f",num1/num2);
-			break;
-	}
+    switch (op)
+    {
+    case '+':
+        printf("%.3f", num1 + num2);
+        break;
+    case '-':
+        printf("%.3f", num1 - num2);
+        break;
+    case '*':
+        printf("%.3f", num1 * num2);
+        break;
+    case '/':
+        printf("%.3f", num1 / num2);
+        break;
+    }
 }
